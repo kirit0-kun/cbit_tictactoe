d1fcb4cbfbb38b59adffbefa4513e6d6e76eaf22
Moustafa Essam <drsh_201122@yahoo.com>
2021-07-23 01:18:53 +0200

    Initial Commit

---
+++ Makefile
@@ -0,0 +1,25 @@
+TARGET=tictactoe
+CC=gcc
+CFLAGS=-g -Wall
+
+LIBS=-lc
+INC=-I.
+
+SRCS=main.c bit_utils.c game.c
+OBJS = $(SRCS:.c=.o)
+
+all: $(TARGET)
+	./${TARGET}
+
+%.o: %.c %.h
+	$(CC) $(CFLAGS) $(INC) -c $< -o $@
+	$(CC) $(CFLAGS) $(INC) -S $^
+
+$(TARGET): $(OBJS)
+	$(CC) $(CFLAGS) $(LIBS) -o $@ $^
+
+clean:
+	rm -f -r *.o $(TARGET) *.s *.h.gch *.dSYM
+
+clear: clean
+	clear
---
+++ game.h
@@ -0,0 +1,33 @@
+#ifndef GAME_HEADER
+#define GAME_HEADER
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+#include "bit_utils.h"
+#include "utils.h"
+
+#define LEN 6
+#define ROWS LEN
+#define COLS LEN
+#define CELL_SIZE 2
+
+#define EMPTY 0b0
+#define X 0b01
+#define O 0b10
+#define INV 0b11
+
+typedef struct Board Board;
+
+Board* create_board();
+void destroy_board(Board* board);
+void set_cell(Board* board, int cell, int value);
+uint8_t get_cell(Board* board, int cell);
+uint8_t get_winner(Board* board);
+uint8_t get_board_size(Board* board);
+int get_player_name(int player, char* output);
+int get_move(int player);
+void print_board(Board* board);
+
+#endif //GAME_HEADER
